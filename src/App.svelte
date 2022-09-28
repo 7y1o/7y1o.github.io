9f591f69b03d871dadf307b695c6d5331821fade
7y1o <tdesu.webdesign@gmail.com>
2022-09-28 16:08:35 +0300

    Updated files

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,25 @@
 <script lang="ts"></script>
+
+<main>
+    <h1>Hello, tylo!</h1>
+</main>
+
+<style lang="sass">
+    main
+      position: fixed
+      z-index: 1
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0
+      display: flex
+      flex-flow: column
+      align-items: center
+      justify-content: center
+
+      h1
+        font: 800 2rem 'Golos'
+        padding: 0
+        margin: 0
+        color: black
+</style>
