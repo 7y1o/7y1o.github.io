f9a71ce9157a9f1e6ba8f734b453f5d5832dcde6
7y1o <tdesu.webdesign@gmail.com>
2022-10-08 17:50:54 +0300

    Created SvelteKit project

--- src/pages/Projects.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  Projects<br/>
-  <router-link to="/">Home</router-link>
-</template>
-
-<!--Style & script-->
-<script lang="ts" src="./script/projects.ts"></script>
-<style scoped></style>
--- src/pages/Skills.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  Skills tree<br/>
-  <router-link to="/">Home</router-link>
-</template>
-
-<!--Style & script-->
-<script lang="ts" src="./script/skills.ts"></script>
-<style scoped></style>
