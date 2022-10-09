a1d8fbdc6c3311402ba8538044e08505ac8f83f2
7y1o <tdesu.webdesign@gmail.com>
2022-10-09 04:56:37 +0300

    Updated pages content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <div class="content">
+        <h1>7y1o</h1>
+        <p>This site is under construction</p>
+    </div>
+</main>
+
+<style lang="scss">
+    main {
+      position: fixed;
+      z-index: 1;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #efefef;
+
+      .content {
+        width: 100%;
+        max-width: 300px;
+        margin: 2rem;
+
+        h1 {
+          font: 900 5rem 'Valky';
+          color: black;
+          text-align: center;
+          padding: 0;
+          margin: 0;
+        }
+
+        p {
+          font: 500 1rem 'Inter';
+          color: rgba(black, .5);
+          text-align: center;
+          padding: 0;
+          margin: 1rem 0 0;
+        }
+      }
+    }
+</style>
